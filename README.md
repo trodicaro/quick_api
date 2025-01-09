c8b7b0dd38f2c1f88d3a526fcfe8f95ed5217b9f
Ro Tr <trodicaro@gmail.com>
2025-01-09 16:08:14 -0600

    Add boto3 and image uploading to S3 bucket

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ python script.py
 ```
 Note: a `.env` file is needed with the following:
 API_KEY=
+AWS_SECRET_ACCESS_KEY=
+AWS_ACCESS_KEY_ID=
+AWS_REGION=
--- script.py
+++ script.py
@@ -3,6 +3,7 @@ import os
 import time
 import urllib
 
+import boto3
 from dotenv import load_dotenv
 import pandas
 import requests 
@@ -10,13 +11,16 @@ from tenacity import retry, stop_after_attempt, wait_exponential
 
 load_dotenv()
 
-# @retry(stop=stop_after_attempt(3), wait=wait_exponential(min=1)) # it waits 2^x * 1 second between each retry; min=1 waits 1s to start retries
+############# PART 1 - API CALLS TO COLLECT DATA #############
+@retry(stop=stop_after_attempt(3), wait=wait_exponential(min=1)) # it waits 2^x * 1 second between each retry; min=1 waits 1s to start retries
 def call_property_endpoint(url, headers, params):
     response = requests.get(url, headers=headers, params=params) # type(response.content) = <class 'bytes'>
 
     response.raise_for_status()
     return response.json() # type(response.json()) = <class 'dict'>
 
+# /property endpoint has more detailed individual data
+# /propertyExtendedSearch returns the property images
 url = "https://zillow-com1.p.rapidapi.com/propertyExtendedSearch"
 headers = {
     'x-rapidapi-key': os.getenv("API_KEY"),
@@ -32,6 +36,8 @@ params = {
 }
 
 current_page = 1
+print(f"Making get request for first page")
+
 response = call_property_endpoint(url, headers, params)
 if response:
 	df = pandas.DataFrame(pandas.json_normalize(response['props']))
@@ -43,11 +49,13 @@ while current_page <= response['totalPages']:
 	try:
 		response = call_property_endpoint(url, headers=headers, params=params)
 		print(f"Current response record count: {response['resultsPerPage']}")
-	except Exception as err:
-		print(f"Error when making the get request: {err.__repr__()}")
+	except Exception as error:
+		print(f"Error when making the get request: {error.__repr__()}")
 	if response:
 		addl_df = pandas.DataFrame(pandas.json_normalize(response['props']))
 		df = pandas.concat([df, addl_df], ignore_index=True) # sub-frame indices are not relevant here
+
+############# PART 2 - DATA MANIPULATION #############
 
 print(f"Total records before applying filters: {len(df)}")
 
@@ -65,6 +73,41 @@ df['specialInterest'] = (df['priceChange'] < 10000) | df['estimateDifference'].g
 
 df.to_csv("data.txt", sep='\t')
 
+############# PART 3 - UPLOADING IMAGES TO AWS S3 #############
+
+s3_client = boto3.client(
+	's3',
+	aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+	aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+	region_name=os.getenv('AWS_REGION')
+)
+s3_bucket = 'quickapi'
+
+for row in df[['carouselPhotos', 'zpid']].itertuples(index=False): # zpid is the unique property identifier on zillow
+	print(f"Starting upload for record {row.zpid}")
+	for index, record in enumerate(row.carouselPhotos):
+		print(f"Currently on image index {index}")
+		# part 1 - load from weblink to a stream file
+		try:
+			response = requests.get(record['url'], stream=True)
+			response.raise_for_status()
+		except Exception as error:
+			print(f"Error loading file: {error}")
+
+		# part 2 - upload to S3
+		s3_filepath = f"{row.zpid} - {index}"
+		try:
+			# s3_client.upload_file(s3_filepath, s3_bucket, object_name)
+			s3_client.put_object(
+				Bucket=s3_bucket,
+				Key=s3_filepath,
+				Body=response.content,
+				ContentType=response.headers.get('Content-Type'),
+				ACL='public-read'
+			)
+			print(f"File uploaded successfully to {s3_bucket}/{s3_filepath}")
+		except Exception as error:
+			print(f"Error uploading file: {error}")
 
 
 
